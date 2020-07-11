31a3d5c357abfc9b0aabc8010ce67db595634dbe
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-11 19:38:43 +0530

    [Mounika] Added:Player then check for a option:Snake,Ladder,Noplay

--- SnakeandLadderGame.sh
+++ SnakeandLadderGame.sh
@@ -1,13 +1,29 @@
 #!/bin/bash -x
 echo "=============Welcome to Snake and ladder program=============="
+NO_PLAY=0
+LADDER=1
+SNAKE=2
 positionOfPlayer=0
-     function diceRoll()
+POSITION=0
+               function diceRoll()
                   {
-                      #while((1))
-                      #do
+                        while((1))
+                        do
                           dieResult=$((RANDOM % 6 + 1))
-                          positionOfPlayer=$(($positionOfPlayer+$dieResult))
-                          echo "positionOfPlayer:$positionOfPlayer"
-                     #done
+                          checkOptions=$((RANDOM % 3))
+                                   case $checkOptions in
+                                        $NO_PLAY)
+                                                   positionOfPlayer=$((positionOfPlayer+0));;
+                                        $LADDER)
+
+                                                   positionOfPlayer=$((positionOfPlayer+$dieResult));;
+                                        $SNAKE)
+
+                                                   positionOfPlayer=$((positionOfPlayer-$dieResult));;
+                                     esac
+
+                                  echo "positionOfPlayer:$positionOfPlayer"
+                    done
                   }
-       diceRoll
+
+              diceRoll
